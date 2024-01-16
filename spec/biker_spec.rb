5979fa9f9bfa904276693d00bafdb13ffa4d18d7
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-16 09:39:11 -0700

    Before :each do setup

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,9 @@ require './lib/biker'
 
 RSpec.describe Biker do
   before :each do
-
+    @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    @biker2 = Biker.new("Athena", 15)
   end
 end
