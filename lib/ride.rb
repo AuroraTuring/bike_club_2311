539e4e5d5a8703cbab74dde6cefbb175a6152c0f
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-16 10:09:07 -0700

    Restructure initialize data to accomodate hashes, #loop?

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,19 +1,23 @@
 class Ride
   attr_reader :name, :distance, :loop, :terrain
 
-  def initialize(name, distance, loop, terrain)
-    @name = name
-    @distance = distance
-    @loop = loop
-    @terrain = terrain
+  def initialize(datahash)
+    @name = datahash[:name]
+    @distance = datahash[:distance]
+    @loop = datahash[:loop]
+    @terrain = datahash[:terrain]
   end
 
-  # def total_distance
-  #   # if the ride is not a loop,
-  #   if loop? == false
-  #     # double the base distance.
-  #       distance * 2
-  #   end
-  # end
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    # if the ride is not a loop,
+    if loop? == false
+      # double the base distance.
+        distance * 2
+    end
+  end
 
 end
